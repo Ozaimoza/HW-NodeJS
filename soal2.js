acf0fd7814ab4c553181d8bde2aceb6085a86ab6
Ozaimoza <mozaikramadhan97@gmail.com>
2023-09-20 21:18:31 +0700

    update soal2.js

--- soal2.js
+++ soal2.js
@@ -1,11 +1,10 @@
 var fs = require ('fs')
 
-fs.readFile("homework.log", "utf-8", (err, data) =>{    //read data homework.log
-    fs.writeFile("log.txt", data, (err) =>{             //write data to log.txt
+fs.readFile("homework.log", "utf-8", (err, data) =>{                             //read data homework.log
+    fs.writeFile("log.txt", data, (err) =>{                                      //write data to log.txt
         if (err) console.log(err)
         console.log("Successfully Written to Log.txt")
         
-        // menampilkan data log.txt
-        fs.readFile("log.txt", "utf-8", (err, data) =>{console.log(data)})
+        // fs.readFile("log.txt", "utf-8", (err, data) =>{console.log(data)})     // Remove comment if you want to show data log.txt in console/terminal
     })
 } )
